78f88a591b522328d54339bb3aa8e4931526327a
Namu-Ujin Natsagnyam <Namz@Namu-Ujins-MacBook-Pro.local>
2015-12-04 21:48:58 +0000

    full screen

--- ageUK/app/src/main/java/com/example/namz/ageuk/VolunteerSignupActivity.java
+++ ageUK/app/src/main/java/com/example/namz/ageuk/VolunteerSignupActivity.java
@@ -14,18 +14,8 @@ public class VolunteerSignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_volunteer_signup);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_volunteer_signup);
     }
 
     @Override
