b7876ca665081eaa7841f90807b6d374445689c5
rosart <tiz@Tsvetans-MacBook-Pro.local>
2015-12-05 12:02:46 +0000

    Example1 server get and send method
    This method is from http://pastebin.com/36weMyj8
    For more information contact it’s creator

---
+++ example1.py
@@ -0,0 +1,94 @@
+package com.example.tiz.myapplication3;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Online get and post to server from http://pastebin.com/36weMyj8
+ * Created by tiz on 05/12/15.
+ */
+public class HttpURLConnectionExample {
+
+        private final String USER_AGENT = "Mozilla/5.0";
+
+
+        // HTTP GET request
+        public String sendGet() throws Exception {
+
+            String url = "http://127.0.0.1/";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+// optional default is GET
+            con.setRequestMethod("GET");
+
+//add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+//print result
+            return response.toString();
+
+        }
+
+        // HTTP POST request
+        private void sendPost() throws Exception {
+
+            String url = "http://127.0.0.1/";
+            URL obj = new URL(url);
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+//add reuqest header
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+            String firstName = "John";
+            String lastName = "Smith";
+            String urlParameters = "firstName="+firstName+"&lastName"+lastName;
+
+// Send post request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Post parameters : " + urlParameters);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+//print result
+            System.out.println(response.toString());
+
+        }
+}
